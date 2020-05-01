1082494dad8888b9dff0d4a40c3a3872b5c231cf
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-05-01 13:58:14 +0900

    embed audio

--- dictionary/main.js
+++ dictionary/main.js
@@ -1,11 +1,34 @@
 "use strict";
+var get_audio_name = function (word) {
+    if (word.entry.form.charAt(0) !== "z") {
+        return word.entry.form;
+    }
+    var pronunciation_info = word.contents.filter(function (_a) {
+        var title = _a.title;
+        return title === "発音";
+    });
+    if (pronunciation_info.length === 0) {
+        return word.entry.form + "_tx"; // defaults to tx
+    }
+    else if (pronunciation_info[0].text.slice(0, 2) === "ts") {
+        return word.entry.form + "_tc";
+    }
+    else {
+        alert("Warning: unexpected pronunciation remark found in the word " + JSON.stringify(word.entry));
+    }
+};
 var get_word = function (id) {
     var word = dictionary.words.filter(function (a) { return a.entry.id === id; })[0];
-    return "<div class=\"word\">\n    <div><div class=\"word_form\">" + word.entry.form + "</div><div class=\"tags\">" + word.tags.map(function (a) { return '<span class="bordered_info">' + a + '</span>'; }).join("") + "</div></div><div class=\"word_infos\">" + word.translations.map(function (t) { return '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>'; }).join("") + "</div>" + word.contents.map(function (_a) {
+    var word_form = "<div><div class=\"word_form\">" + word.entry.form + "</div><div class=\"tags\">" + word.tags.map(function (a) { return '<span class="bordered_info">' + a + '</span>'; }).join("") + "</div></div>";
+    var translations = word.translations.map(function (t) { return '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>'; }).join("");
+    var audio = word.entry.form.indexOf(" ") !== -1 ? "" : " <p class=\"word_info\"><span class=\"bordered_info\">\u97F3\u58F0</span><audio controls><source src=\"../audio/" + get_audio_name(word) + ".mp3\" type=\"audio/mpeg\">\n    Your browser does not support the audio element.\n    </audio></p>";
+    var audio_and_translations = "<div class=\"word_infos\">" + (audio + translations) + "</div>";
+    var contents = word.contents.map(function (_a) {
         var title = _a.title, text = _a.text;
         return '<div class="word_infos"><p class="word_info"><span class="nonbordered_info">' + title + '</span>' + text + '</p></div>';
-    }).join("") + "</div>";
+    }).join("");
+    return "<div class=\"word\">" + (word_form + audio_and_translations + contents) + "</div>";
 };
 var render = function () {
-    document.getElementById("outer").innerHTML = [get_word(342), get_word(2), get_word(1)].join("");
+    document.getElementById("outer").innerHTML = [get_word(140), get_word(141), get_word(342), get_word(2), get_word(1)].join("");
 };
--- dictionary/main.ts
+++ dictionary/main.ts
@@ -1,16 +1,40 @@
+const get_audio_name = (word: {
+    entry: {id: number, form: string},
+    contents: {title: string, text: string}[]
+}) => {
+    if (word.entry.form.charAt(0) !== "z") {
+        return word.entry.form;
+    } 
+    const pronunciation_info = word.contents.filter(({title}) => title === "発音");
+    if (pronunciation_info.length === 0) {
+        return word.entry.form + "_tx"; // defaults to tx
+    } else if (pronunciation_info[0].text.slice(0, 2) === "ts") {
+        return word.entry.form + "_tc"; 
+    } else {
+        alert("Warning: unexpected pronunciation remark found in the word " + JSON.stringify(word.entry))
+    }
+}
+
 const get_word = (id: number) => {
     const [word] = dictionary.words.filter(a => a.entry.id === id);
-    
-    return `<div class="word">
-    <div><div class="word_form">${word.entry.form}</div><div class="tags">${
+
+    const word_form = `<div><div class="word_form">${word.entry.form}</div><div class="tags">${
         word.tags.map(a => '<span class="bordered_info">' + a + '</span>').join("")
-    }</div></div><div class="word_infos">${
-        word.translations.map(t => '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>').join("")
-    }</div>${
-        word.contents.map(({title, text}) => '<div class="word_infos"><p class="word_info"><span class="nonbordered_info">'+title+'</span>'+text+'</p></div>').join("")
-    }</div>`;
+    }</div></div>`;
+
+    const translations = word.translations.map(t => '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>').join("");
+
+    const audio = word.entry.form.indexOf(" ") !== -1 ? "" : ` <p class="word_info"><span class="bordered_info">音声</span><audio controls><source src="../audio/${get_audio_name(word)}.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+    </audio></p>`;
+
+    const audio_and_translations = `<div class="word_infos">${audio + translations}</div>`;
+
+    const contents = word.contents.map(({title, text}) => '<div class="word_infos"><p class="word_info"><span class="nonbordered_info">'+title+'</span>'+text+'</p></div>').join("");
+
+    return `<div class="word">${word_form + audio_and_translations + contents}</div>`;
 }
 
 const render = () => {
-    document.getElementById("outer")!.innerHTML = [get_word(342), get_word(2), get_word(1)].join("");
+    document.getElementById("outer")!.innerHTML = [get_word(140), get_word(141), get_word(342), get_word(2), get_word(1)].join("");
 }
